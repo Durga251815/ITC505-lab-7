f0f159b14854a9f43ae07878cb8140fceaf0f61c
Durga251815 <158021017+Durga251815@users.noreply.github.com>
2024-04-15 11:40:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/cs212/homework/8', (req, res) => {
 // Your POST route for /cs212/homework/8
 app.post('/cs212/homework/8', (req, res) => {
     const { adjective, pluralNoun, personName, verb, noun } = req.body;
-    const madLib = `Today, I saw a ${adjective} ${pluralNoun} which made me think of ${personName}. 
-                    Then I decided to ${verb} by the ${noun}.`;
+    const madLib = "Yesterday, I found an ${adjective} ${pluralNoun} that reminded me of ${personName}. 
+                    So, I chose to ${verb} near the ${noun}.";
 
     res.send(madLib);
 });
